03599897190406f87fee7a027403d5f257a92a69
isdelr <rosvelt005@hotmail.com>
2025-07-20 16:32:52 -0400

    changed middleware

--- middleware.ts
+++ middleware.ts
@@ -4,25 +4,23 @@ import {
   nextjsMiddlewareRedirect,
 } from "@convex-dev/auth/nextjs/server";
 
-// All routes except the landing page and the sign-in page are protected.
-const isProtectedRoute = createRouteMatcher([
-  "/active-rounds(.*)",
-  "/bookmarked(.*)",
-  "/explore(.*)",
-  "/leagues/(.*)",
-  "/my-submissions(.*)",
-  "/server(.*)",
+// Define the routes that are publicly accessible.
+// All other routes will require authentication.
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/signin",
 ]);
 
 export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
-  if (isProtectedRoute(request) && !(await convexAuth.isAuthenticated())) {
-    // Redirect unauthenticated users from protected pages to the sign-in page.
+  // If the route is not public and the user is not authenticated,
+  // redirect them to the signin page.
+  if (!isPublicRoute(request) && !(await convexAuth.isAuthenticated())) {
     return nextjsMiddlewareRedirect(request, "/signin");
   }
 });
 
 export const config = {
   // The following matcher runs middleware on all routes
-  // except static assets.
+  // except for static files and Next.js-specific assets.
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
