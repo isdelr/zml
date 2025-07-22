56ebcd26f09d00db90420361a528c780a60deb43
isdelr <rosvelt005@hotmail.com>
2025-07-22 11:39:21 -0400

    switched home page navbar logo

--- components/home/HomeHeader.tsx
+++ components/home/HomeHeader.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Music } from "lucide-react";
+import { AudioLines} from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { useAuthActions } from "@convex-dev/auth/react";
@@ -12,7 +12,7 @@ export function HomeHeader() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex mx-auto h-14 max-w-screen-2xl items-center justify-between">
         <Link href="/" className="flex items-center gap-2">
-          <Music className="size-6 text-primary" />
+          <AudioLines className="size-6 text-primary" />
           <span className="font-bold">ZML</span>
         </Link>
         <nav className="flex items-center gap-4">
