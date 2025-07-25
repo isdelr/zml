00d053a62398f209780cd365f0c655ace76dbbb4
isdelr <rosvelt005@hotmail.com>
2025-07-25 09:24:45 -0400

    fixed build issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,30 @@
-"use client";
-
-import { useConvexAuth } from "convex/react";
-import { LoadingSpinner } from "@/components/LoadingSpinner";
-import { dynamicImport } from "@/components/ui/dynamic-import";
+// No "use client" directive - this is now a Server Component.
 import { PageLayout } from "@/components/layout/PageLayout";
 import { ExplorePage } from "@/components/ExplorePage";
+import { preloadQuery } from "convex/nextjs";
+import { api } from "@/convex/_generated/api";
 
- 
-const HomeHeader = dynamicImport(() =>
-  import("@/components/home/HomeHeader").then((mod) => ({
-    default: mod.HomeHeader,
-  })),
-);
-const HomeHero = dynamicImport(() =>
-  import("@/components/home/HomeHero").then((mod) => ({
-    default: mod.HomeHero,
-  })),
-);
-const HomeFeatures = dynamicImport(() =>
-  import("@/components/home/HomeFeatures").then((mod) => ({
-    default: mod.HomeFeatures,
-  })),
-);
-const HomeFooter = dynamicImport(() =>
-  import("@/components/home/HomeFooter").then((mod) => ({
-    default: mod.HomeFooter,
-  })),
-);
+// Direct imports for the unauthenticated view
+import { HomeHeader } from "@/components/home/HomeHeader";
+import { HomeHero } from "@/components/home/HomeHero";
+import { HomeFeatures } from "@/components/home/HomeFeatures";
+import { HomeFooter } from "@/components/home/HomeFooter";
+import { isAuthenticatedNextjs } from "@convex-dev/auth/nextjs/server";
 
-export default function HomePage() {
-  const { isAuthenticated, isLoading } = useConvexAuth();
+export default async function HomePage() {
+  const userIsAuthenticated = await isAuthenticatedNextjs();
 
-  if (isLoading) {
-    return <LoadingSpinner />;
-  }
-
-   
-   
-   
-  if (isAuthenticated) {
+  // If the user is authenticated, show the Explore page with preloaded data.
+  if (userIsAuthenticated) {
+    const preloadedLeagues = await preloadQuery(api.leagues.getPublicLeagues);
     return (
       <PageLayout>
-        <ExplorePage />
+        <ExplorePage preloadedLeagues={preloadedLeagues} />
       </PageLayout>
     );
   }
 
-   
+  // If the user is not authenticated, show the marketing landing page.
   return (
     <div className="flex min-h-screen flex-col bg-background">
       <HomeHeader />
